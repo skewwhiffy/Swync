8859bf639d070bab8a2e74069a7f9e7af677a49e
Kenny Hung <kennyhung@live.co.uk>
2018-05-07 21:19:49 +0100

    Remove Onedrive SDKs

--- test/Swync.integration/AuthenticationTests.cs
+++ test/Swync.integration/AuthenticationTests.cs
@@ -1,10 +1,3 @@
-using System.IO;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Microsoft.Extensions.Configuration;
-using Microsoft.OneDrive.Sdk;
-using Swync.core.Authentication;
-using Swync.integration.Config;
 using Xunit;
 
 namespace Swync.integration
