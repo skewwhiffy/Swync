56bdfc8b69892a7bcf81f5fd9bd0473875cd28bd
Kenny Hung <kenny.hung@oliverwyman.com>
2018-05-07 09:46:56 +0100

    Refresh token, actually working

--- src/Swync.core/Authentication/Authenticator.cs
+++ src/Swync.core/Authentication/Authenticator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
@@ -13,26 +14,29 @@ namespace Swync.core.Authentication
 {
     public class Authenticator
     {
-        private const string ClientId = "23aec4fb-a025-46f7-beba-02caeac8b791";
+        private const string ClientId = "21133f26-e5d8-486b-8b27-0801db6496a9";
         private static readonly int[] PortsRegisteredWithMicrosoft = {80, 8080, 38080};
 
         private static readonly string[] Scopes = {
             "files.readwrite",
             "offline_access"
         };
-        
+
+        private int? _port;
+        private int Port => _port ?? (_port = GetFreePort()).Value;
+        private string Callback => $"http://localhost:{Port}";
+       
         private const string ResponseType = "code";
         
         public async Task<string> GetAuthorizationCodeAsync()
         {
-            var port = GetFreePort();
-            var listenTask = ListenForAuthorizationCode(port);
-            var callback = $"http://localhost:{port}";
+            return "M39eba934-f3e7-62ed-e89d-60e0ffb678f6";
+            var listenTask = ListenForAuthorizationCode(Port);
             var queryVariables = new Dictionary<string, string>
             {
                 {"client_id", ClientId},
                 {"scope", Scopes.Join(" ")},
-                {"redirect_uri", callback},
+                {"redirect_uri", Callback},
                 {"response_type", ResponseType}
             };
             var queryVariablesString = queryVariables
@@ -42,6 +46,28 @@ namespace Swync.core.Authentication
             OpenBrowser($"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?{queryVariablesString}");
             
             return await listenTask;
+        }
+
+        public async Task<string> GetRefreshTokenAsync(string authToken)
+        {
+            var queryVariables = new Dictionary<string, string>
+            {
+                {"client_id", ClientId},
+                {"redirect_uri", Callback},
+                {"code", authToken},
+                {"grant_type", "authorization_code"},
+                {"client_secret", "gcyhkJZK73!$:zqHNBE243}"}
+            };
+            var postPayload = queryVariables
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Pipe(HttpUtility.UrlEncode))
+                .Select(kvp => $"{kvp.Key}={kvp.Value}")
+                .Join("&");
+            using (var client = new HttpClient())
+            {
+                var content = new FormUrlEncodedContent(queryVariables);
+                var response = await client.PostAsync("https://login.microsoftonline.com/common/oauth2/v2.0/token", content);
+                return await response.Content.ReadAsStringAsync();
+            }
         }
         
         private int GetFreePort()
--- test/Swync.integration/IntegrationTestBase.cs
+++ test/Swync.integration/IntegrationTestBase.cs
@@ -11,7 +11,9 @@ namespace Swync.integration
             var codeTask = authenticator.GetAuthorizationCodeAsync();
             codeTask.Wait();
             var code = codeTask.Result;
-            true.Should().BeFalse(code);
+            var refreshTokenTask = authenticator.GetRefreshTokenAsync(code);
+            refreshTokenTask.Wait();
+            true.Should().BeFalse($"Authorization code is ${code}, refresh token is ${refreshTokenTask.Result}");
         }
     }
 }
