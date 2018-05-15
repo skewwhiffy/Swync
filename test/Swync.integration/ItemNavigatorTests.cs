2dcc34846b86bca9b79064ceaa254aa0eaab251e
Kenny Hung <kennysubs+github@outlook.com>
2018-05-15 22:48:37 +0100

    Intercepting HttpClient

--- test/Swync.integration/ItemNavigatorTests.cs
+++ test/Swync.integration/ItemNavigatorTests.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -11,6 +9,7 @@ using Swync.core.Onedrive.Authentication;
 using Swync.core.Onedrive.Http;
 using Swync.core.Onedrive.Items;
 using Swync.test.common.Extensions;
+using Swync.test.common.Interceptors;
 using Xunit;
 
 namespace Swync.integration
@@ -18,16 +17,15 @@ namespace Swync.integration
     public class ItemNavigatorTests : IntegrationTestBase
     {
         private readonly bool _exhaustive;
-        private readonly Dictionary<HttpRequestMessage, string> _getRequests;
         private readonly ItemNavigator _sut;
+        private readonly HttpInterceptor _interceptingClient;
 
         public ItemNavigatorTests()
         {
-            _getRequests = new Dictionary<HttpRequestMessage, string>();
             IAuthenticator authenticator = new Authenticator();
             var mockClientFactory = new Mock<IHttpClientFactory>();
-            var interceptingClient = new InterceptingClient(_getRequests);
-            mockClientFactory.Setup(f => f.GetClient()).Returns(interceptingClient);
+            _interceptingClient = new HttpInterceptor();
+            mockClientFactory.Setup(f => f.GetClient()).Returns(_interceptingClient);
             var access = new OnedriveAuthenticatedAccess(authenticator, mockClientFactory.Object);
             _sut = new ItemNavigator(access);
 
@@ -41,53 +39,28 @@ namespace Swync.integration
         public async Task CanNavigateChildren()
         {
             var drives = await _sut.GetDrivesAsync(CancellationToken.None);
-            var drivesPayload = _getRequests.Values.Single();
+            var drivesPayload =_interceptingClient.Cache.Values.Single();
             drives.SerializeToPrettyJson().Should().Be(drivesPayload.PrettyJson());
             
-            _getRequests.Clear();
+            _interceptingClient.ClearCache();
 
             var drivesToTest = _exhaustive ? drives.value : new[] {drives.value.TakeRandom()};
             foreach (var drive in drivesToTest)
             {
                 var itemsAtRoot = await _sut.GetItemsAsync(drive, CancellationToken.None);
-                var directoriesPayload = _getRequests.Values.Single();
+                var directoriesPayload = _interceptingClient.Cache.Values.Single();
                 itemsAtRoot.SerializeToPrettyJson().Should().Be(directoriesPayload.PrettyJson());
                 
-                _getRequests.Clear();
+                _interceptingClient.ClearCache();
 
                 var foldersToTest = _exhaustive ? itemsAtRoot.value : new[] {itemsAtRoot.value.TakeRandom()};
                 foreach (var folderAtRoot in foldersToTest)
                 {
                     var childItems = await _sut.GetChildrenAsync(folderAtRoot.id, CancellationToken.None);
-                    var childItemsPayload = _getRequests.Values.Single();
+                    var childItemsPayload = _interceptingClient.Cache.Values.Single();
                     childItems.SerializeToPrettyJson().Should().Be(childItemsPayload.PrettyJson());
                     
-                    _getRequests.Clear();
-                }
-            }
-        }
-
-        class InterceptingClient : IHttpClient
-        {
-            private readonly Dictionary<HttpRequestMessage, string> _cache;
-            
-            public InterceptingClient(Dictionary<HttpRequestMessage, string> cache)
-            {
-                _cache = cache;
-            }
-            
-            public void Dispose()
-            {
-            }
-
-            public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage message, CancellationToken ct)
-            {
-                using (var client = new HttpClient())
-                {
-                    var response = await client.SendAsync(message, ct);
-                    var payload = await response.Content.ReadAsStringAsync();
-                    _cache[message] = payload;
-                    return response;
+                    _interceptingClient.ClearCache();
                 }
             }
         }
---
+++ test/Swync.test.common/Interceptors/HttpInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Swync.core.Onedrive.Http;
+
+namespace Swync.test.common.Interceptors
+{
+    public class HttpInterceptor : IHttpClient
+    {
+        private readonly ConcurrentDictionary<HttpRequestMessage, string> _cache;
+
+        public HttpInterceptor()
+        {
+            _cache = new ConcurrentDictionary<HttpRequestMessage, string>();
+        }
+        
+        public void Dispose()
+        {
+        }
+
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage message, CancellationToken ct)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.SendAsync(message, ct);
+                var payload = await response.Content.ReadAsStringAsync();
+                _cache[message] = payload;
+                return response;
+            }
+        }
+
+        public ImmutableDictionary<HttpRequestMessage, string> Cache => _cache
+            .ToImmutableDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+        public void ClearCache() => _cache.Clear();
+    }
+}
